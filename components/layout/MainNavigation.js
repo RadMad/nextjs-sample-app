ab34aa6c499d318ce7155c6ada54b168c6cfbae6
Anton Radtsevich <aradtsevich@productiveedge.com>
2022-01-06 18:06:33 +0300

    added authentication auth0

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,8 +1,10 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import classes from './MainNavigation.module.css';
+import classes from "./MainNavigation.module.css";
+import { useUser } from "@auth0/nextjs-auth0";
 
 function MainNavigation() {
+  const { user, isLoading } = useUser();
 
   return (
     <header className={classes.header}>
@@ -10,11 +12,14 @@ function MainNavigation() {
       <nav>
         <ul>
           <li>
-            <Link href='/'>All Meetups</Link>
+            <Link href="/">All Meetups</Link>
           </li>
           <li>
-            <Link href='/new-meetup'>Add New Meetup</Link>
+            <Link href="/new-meetup">Add New Meetup</Link>
           </li>
+          <li>{!isLoading && !user && <a href="/api/auth/login">Log in</a>}</li>
+          <li>{user && <span>{user.name}</span>}</li>
+          <li>{user && <a href="/api/auth/logout">Log out</a>}</li>
         </ul>
       </nav>
     </header>
