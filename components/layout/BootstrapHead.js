000a6305a1c8568f737c77ef219280ff4d1d321c
Anton Radtsevich <aradtsevich@productiveedge.com>
2022-01-14 19:46:30 +0300

    build fix

--- components/layout/BootstrapHead.js
+++ components/layout/BootstrapHead.js
@@ -13,7 +13,7 @@ function BootstrapHead(props) {
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
         rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-        crossOgit rigin="anonymous"
+        crossOrigin="anonymous"
       ></link>
     </Head>
   );
