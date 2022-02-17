9a20b600e14dd196d1cf5ac1c0c989c99abdf381
Anton Radtsevich <aradtsevich@productiveedge.com>
2022-02-17 17:00:58 +0300

    added loading indicator - nprogress package

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,15 @@ import Layout from "../components/layout/Layout";
 import "../styles/globals.css";
 import { UserProvider } from "@auth0/nextjs-auth0";
 
+import Router from "next/router";
+import NProgress from "nprogress"; //nprogress module
+import "nprogress/nprogress.css"; //styles of nprogress
+
+//Binding events.
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
 function MyApp({ Component, pageProps }) {
   return (
     <UserProvider>
